2cf1067fea1e9c69858194d4f33b92b528fb5275
wangqiang <wang_qiang-neu@neusoft.com>
2020-10-10 15:59:57 +0900

    新建鸿蒙工程

---
+++ app/src/main/java/id/zelory/compressor/Compressor.java
@@ -0,0 +1,10 @@
+package id.zelory.compressor;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class Compressor extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
---
+++ app/src/test/java/id/zelory/compressor/MainAbilityTest.java
@@ -0,0 +1,9 @@
+package id.zelory.compressor;
+
+import org.junit.Test;
+
+public class MainAbilityTest {
+    @Test
+    public void onStart() {
+    }
+}
