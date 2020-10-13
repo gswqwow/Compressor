448750a965fbc19d4227a0327e5933f34bcb0f87
“93686284@qq.com <93686284@qq.com>
2020-10-13 11:46:28 +0800

    单元测试移植试验

---
+++ compressor/src/test/java/id.zelory.compressor/constraint/CompressionTest.java
@@ -0,0 +1,20 @@
+package id.zelory.compressor.constraint;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+public final class CompressionTest {
+    @Test
+    public final void add_constraint_should_save_it_to_constraint_list() {
+        Compression compression = new Compression();
+
+        Constraint mockk = mock(DefaultConstraint.class);
+        compression.constraint(mockk);
+        compression.constraint(mockk);
+
+        // Then
+        assertEquals(compression.constraints.size(), 2);
+    }
+}
