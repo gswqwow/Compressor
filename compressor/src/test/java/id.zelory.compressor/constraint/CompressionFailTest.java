ed75ef181cd1e1575e5c7e08b351493cb31e6b52
马青松 <maqs@neusoft.com>
2020-10-14 15:34:38 +0800

    logger测试和Hilogger测试。junit中无法使用hilog

--- compressor/src/test/java/id.zelory.compressor/constraint/CompressionFailTest.java
+++ compressor/src/test/java/id.zelory.compressor/constraint/CompressionFailTest.java
@@ -2,13 +2,20 @@ package id.zelory.compressor.constraint;
 
 import org.junit.Test;
 
+import java.util.logging.Logger;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class CompressionFailTest {
+    private static final Logger logger = Logger.getLogger(CompressionFailTest.class.getName());
+    //private static final HiLogLabel label = new HiLogLabel(HiLog.LOG_APP, 0x00201, "MY_TAG");
+
     @Test
     public void testFail() {
-        fail("exp");
+        //fail("exp");
+        logger.info("mytest");
+        //HiLog.warn(label, "Failed to visit %{private}s, reason:%{public}d.", "www.baidu.com", "123");
     }
     @Test
     public void testok() {
