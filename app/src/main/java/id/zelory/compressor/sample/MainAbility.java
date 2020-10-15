eef0636a79999fa74a4b9ab45536bc6b547136cd
wangqiang <wang_qiang-neu@neusoft.com>
2020-10-15 10:32:01 +0900

    添加布局文件

--- app/src/main/java/id/zelory/compressor/sample/MainAbility.java
+++ app/src/main/java/id/zelory/compressor/sample/MainAbility.java
@@ -1,13 +1,21 @@
 package id.zelory.compressor.sample;
 
+import id.zelory.compressor.ResourceTable;
 import id.zelory.compressor.sample.slice.MainAbilitySlice;
 import ohos.aafwk.ability.Ability;
 import ohos.aafwk.content.Intent;
+import ohos.hiviewdfx.HiLog;
+import ohos.hiviewdfx.HiLogLabel;
 
 public class MainAbility extends Ability {
+
+    static final HiLogLabel label = new HiLogLabel(HiLog.LOG_APP, 0x0, "MY_TAG");
+
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
-        super.setMainRoute(MainAbilitySlice.class.getName());
+        super.setUIContent(ResourceTable.Layout_Ability_main);
     }
+
+
 }
