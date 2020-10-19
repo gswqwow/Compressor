03d117af83978fc1afafbb267f7eb1966513cd10
马青松 <maqs@neusoft.com>
2020-10-19 15:51:36 +0800

    mock jar 更换为可以 使用 final mock的 mockito-inline

--- compressor/src/test/java/id.zelory.compressor/constraint/CompressionTest.java
+++ compressor/src/test/java/id.zelory.compressor/constraint/CompressionTest.java
@@ -1,15 +1,21 @@
 package id.zelory.compressor.constraint;
 
+import ohos.hiviewdfx.HiLog;
+import ohos.hiviewdfx.HiLogLabel;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public final class CompressionTest {
     @Test
     public final void add_constraint_should_save_it_to_constraint_list() {
         Compression compression = new Compression();
 
+        HiLogLabel label = mock(HiLogLabel.class);
+        when(HiLog.error(any(),any()));
         Constraint mock = mock(DefaultConstraint.class);
         compression.constraint(mock);
         compression.constraint(mock);
