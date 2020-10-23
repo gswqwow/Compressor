8a314c4a9cc8262e5ed146903694289ca6c646cd
李显众 <lixianzhong@neusoft.com>
2020-10-23 18:42:52 +0800

    compressor测试修改

--- compressor/src/test/java/id.zelory.compressor/CompressorTest.java
+++ compressor/src/test/java/id.zelory.compressor/CompressorTest.java
@@ -1,16 +1,11 @@
 package id.zelory.compressor;
 
-import id.zelory.compressor.constraint.Compression;
-import id.zelory.compressor.constraint.Constraint;
-import id.zelory.compressor.constraint.DefaultConstraint;
+import id.zelory.compressor.constraint.*;
 import ohos.app.Context;
-import org.junit.Assert;
 import ohos.media.image.ImageSource;
-import ohos.media.image.common.ImageInfo;
-import ohos.media.image.common.Size;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -53,30 +48,55 @@ public class CompressorTest {
         Compressor compressor = new Compressor();
         File compressFile = compressor.compress(context, file, compression);
         Assert.assertEquals(file,compressFile);
-        //Mockito.verify(compression).compressionDefault(file);
     }
 
     @Test
-    public void compress_with_custom_specs_should_execute_all_constraint_provided() throws Exception {
-        // null参数时mock示例
+    public void compress_with_default_specs_should_execute_constraints() throws Exception {
+        // 传参mock示例
         DefaultConstraint dc = PowerMockito.mock(DefaultConstraint.class);
-        Context context = PowerMockito.mock(Context.class);
+        DestinationConstraint dec = PowerMockito.mock(DestinationConstraint.class);
+        FormatConstraint fc = PowerMockito.mock(FormatConstraint.class);
+        QualityConstraint qc = PowerMockito.mock(QualityConstraint.class);
+        ResolutionConstraint rc = PowerMockito.mock(ResolutionConstraint.class);
+        SizeConstraint sc = PowerMockito.mock(SizeConstraint.class);
+
+        Compression compression  = PowerMockito.mock(Compression.class);
         File file = new File("/");
 
-        ImageSource imageSource = PowerMockito.mock(ImageSource.class);
-        PowerMockito.mockStatic(ImageSource.class);
-        PowerMockito.when(ImageSource.create(file, null)).thenReturn(imageSource);
-
-        Size size = PowerMockito.mock(Size.class);
-        size.height = 100;
-        size.height = 100;
-        ImageInfo imageInfo = PowerMockito.mock(ImageInfo.class);
-        imageInfo.size = size;
-        PowerMockito.when(imageSource.getImageInfo()).thenReturn(imageInfo);
-
         PowerMockito.whenNew(DefaultConstraint.class).withAnyArguments().thenReturn(dc);
-        Compressor compressor = new Compressor();
-        compressor.compress(context, file, null);
+        PowerMockito.whenNew(Compression.class).withAnyArguments().thenReturn(compression);
 
+        PowerMockito.doNothing().when(compression).compressionDefault(file);
+
+        List<Constraint> list =  new ArrayList<>();
+        list.add(dc);
+        list.add(dec);
+        list.add(fc);
+        list.add(qc);
+        list.add(rc);
+        list.add(sc);
+
+        PowerMockito.when(compression.getConstraints()).thenReturn(list);
+
+        PowerMockito.when(dc.isSatisfied(any())).thenReturn(true);
+        PowerMockito.when(dc.satisfy(any())).thenReturn(file);
+        PowerMockito.when(dec.isSatisfied(any())).thenReturn(true);
+        PowerMockito.when(dec.satisfy(any())).thenReturn(file);
+        PowerMockito.when(fc.isSatisfied(any())).thenReturn(true);
+        PowerMockito.when(fc.satisfy(any())).thenReturn(file);
+        PowerMockito.when(qc.isSatisfied(any())).thenReturn(true);
+        PowerMockito.when(qc.satisfy(any())).thenReturn(file);
+        PowerMockito.when(rc.isSatisfied(any())).thenReturn(true);
+        PowerMockito.when(rc.satisfy(any())).thenReturn(file);
+        PowerMockito.when(sc.isSatisfied(any())).thenReturn(true);
+        PowerMockito.when(sc.satisfy(any())).thenReturn(file);
+
+        Context context = PowerMockito.mock(Context.class);
+        PowerMockito.mockStatic(Util.class);
+        PowerMockito.when(Util.copyToCache(context, file)).thenReturn(file);
+
+        Compressor compressor = new Compressor();
+        File compressFile = compressor.compress(context, file, compression);
+        Assert.assertEquals(file,compressFile);
     }
 }
